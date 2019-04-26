62ab473053daf4c443c0dec8d6e56b9e8c3fc819
alxfed <alxfed@gmail.com>
2019-04-26 18:45:46 -0500

    Socrata with separately stored token initial

---
+++ work_with_socrata.py
@@ -0,0 +1,19 @@
+"""
+Working with socrata where some of the data is
+"""
+
+
+from sodapy import Socrata
+import pandas as pd
+
+with open('socrata_API_token.txt', 'r') as token_file: # zoning and permits app
+    token = token_file.read(25)
+    token_file.close()
+
+with Socrata("data.cityofchicago.org", token) as client:
+    re = client.get("building-permits", limit=2)
+    client.close()
+
+sm = pd.DataFrame(re)
+
+print(sm)
