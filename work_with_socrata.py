93823d07cd50ed06033f734c47945159efd66598
alxfed <alxfed@gmail.com>
2019-04-26 19:40:50 -0500

    rstrip

--- work_with_socrata.py
+++ work_with_socrata.py
@@ -7,7 +7,7 @@ from sodapy import Socrata
 import pandas as pd
 
 with open('socrata_API_token.txt', 'r') as token_file: # zoning and permits app
-    token = token_file.read(25)
+    token = token_file.read().rstrip('\n')
     token_file.close()
 
 with Socrata("data.cityofchicago.org", token) as client:
