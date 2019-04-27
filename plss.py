b1437924fa1e3fc71fb894a3a9473634f405729b
alxfed <alxfed@gmail.com>
2019-04-27 16:54:15 -0500

    added Parcels-2016

---
+++ plss.py
@@ -0,0 +1,14 @@
+"""
+PLSS Line
+The County's system of tax maps is based on the Illinois
+Public Land Survey System (PLSS). In the PLSS, each township
+is divided into 36 sections, and each section into four
+quarter-sections. A quarter-section is further divided into
+two tax map sheets, often called "pages". Each tax map
+(1/4 mile by 1/2 mile) represents the east or west half of
+one quarter-section, and typically there are eight tax maps
+per section.
+"""
+
+# Survey Township   https://datacatalog.cookcountyil.gov/resource/iz5k-qtpu.json
+# PLSS Line         https://datacatalog.cookcountyil.gov/resource/yvw8-g53g.json
