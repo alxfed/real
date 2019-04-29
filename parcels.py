82dc2e607602d2bac098c222471af49ff30f35b8
alxfed <alxfed@gmail.com>
2019-04-29 08:04:05 -0500

    trying

--- parcels.py
+++ parcels.py
@@ -17,11 +17,10 @@ CC_END_POINT_DOMAIN = 'datacatalog.cookcountyil.gov' # with leading https://
 
 '''
 Parcel 2000 Field Names:
-the_geom, the_geom, 
-PIN10, pin10, PIN14, pin14, PINA, pina, PINSA, pinsa, PINB, pinb, PINP, pinp, PINU, pinu,PINAC, pinac,
-TAXCODE, taxcode, JOB_NO, job_no, 
-PARCELTYPE, parceltype, UPPER_ELEV, upper_elev, LOWER_ELEV, lower_elev,
-SHAPE_area, shape_area, SHAPE_len, shape_len
+the_geom, 
+pin10, pin14, pina, pinsa, pinb, pinp, pinu, pinac,
+taxcode, job_no, parceltype, upper_elev, lower_elev,
+shape_area, shape_len
 '''
 
 CCGISDATA_PARCEL_2000 = 'bbcr-ryng' # map page/GIS-Maps/ccgisdata-Parcel-2000/x2ib-rt29
