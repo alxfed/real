ee2570e345dde32c8c74479702d891a0e6d59978
alxfed <alxfed@gmail.com>
2019-04-29 08:28:32 -0500

    adding more parcel datasets

--- parcels.py
+++ parcels.py
@@ -26,10 +26,14 @@ shape_area, shape_len
 CCGISDATA_PARCEL_2000 = 'bbcr-ryng' # map page /GIS-Maps/ccgisdata-Parcel-2000/x2ib-rt29
 CCGISDATA_PARCEL_2001 = 't799-vjyq' # map page /GIS-Maps/ccgisdata-Parcel-2001/i2km-7cwi
 CCGISDATA_PARCEL_2002 = 'dckf-92bv' # map page /GIS-Maps/ccgisdata-Parcel-2002/3wb8-2she
+CCGISDATA_PARCEL_2003 = 'anwy-2bet' # map page /GIS-Maps/ccgisdata-Parcel-2003/gcqr-kbqq
+CCGISDATA_PARCEL_2004 = 'sg9a-x6ka' # map page /GIS-Maps/ccgisdata-Parcel-2004/6jt3-ndyu
+CCGISDATA_PARCEL_2005 = '7emi-v3x2' # map page /GIS-Maps/ccgisdata-Parcel-2005/f42p-dbfm
 
 
 # The list of datasets
-AVAILABLE_DATASETS = [CCGISDATA_PARCEL_2000, CCGISDATA_PARCEL_2001, ]
+AVAILABLE_DATASETS = [CCGISDATA_PARCEL_2000, CCGISDATA_PARCEL_2001, CCGISDATA_PARCEL_2002,
+                      CCGISDATA_PARCEL_2003, CCGISDATA_PARCEL_2004, CCGISDATA_PARCEL_2002]
 
 
 from sodapy import Socrata
