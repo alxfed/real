941a7001df6c655bce9acd246155f138e80664fc
alxfed <alxfed@gmail.com>
2019-04-29 07:31:05 -0500

    constants for parcels

---
+++ parcels.py
@@ -0,0 +1,12 @@
+"""
+Lots and the history of parcels, mergers and splits
+
+data is from datacatalog cookcountyil gov
+"""
+
+# constants
+
+DOMAIN = 'datacatalog.cookcountyil.gov' # with leading https://
+
+CCGISDATA_PARCEL_2000 = 'bbcr-ryng' # map /GIS-Maps/ccgisdata-Parcel-2000/x2ib-rt29
+
