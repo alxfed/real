1160e2b46acfc6bea8025ab3b9658fdfbbf2085c
alxfed <alxfed@gmail.com>
2019-05-05 20:32:28 -0500

    datetime

--- socrata_datetime.py
+++ socrata_datetime.py
@@ -8,4 +8,6 @@ from dateutil.parser import parse
 datet = parse('2011-01-03T01:02:01')
 backs = datet.strftime('%Y-%m-%dT%H:%M:%S')
 
-print(datet)
+in_a_query = f"where={backs!r}"
+
+print(in_a_query)
