8ac7dfc1055002b585e3086cc18818944d30c9f0
alxfed <alxfed@gmail.com>
2019-05-05 19:57:06 -0500

    datetime

--- socrata_datetime.py
+++ socrata_datetime.py
@@ -1,9 +1,10 @@
 """
-Dealing with the "Floating Timestamp Datatype
+Dealing with the socrata "Floating Timestamp" datatype
 """
 
 from dateutil.parser import parse
 
 datet = parse('2011-01-03T01:02:01')
+backs = datet.strftime('%Y-%m-%dT%H:%M:%S')
 
 print(datet)
