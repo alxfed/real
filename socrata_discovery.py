28d3c4e802dfab1d5cf8e4dae472fc4eb642d1fc
alxfed <alxfed@gmail.com>
2019-05-08 17:06:58 -0500

    discovery

--- socrata_discovery.py
+++ socrata_discovery.py
@@ -2,6 +2,16 @@
 Socrata discovery API
 """
 
+# facets of a domain
+# http://api.us.socrata.com/api/catalog/v1/domains/domain/facets
+
+# in particular
+# http://api.us.socrata.com/api/catalog/v1/domains/datacatalog.cookcountyil.gov/facets
+
+# and
+# http://api.us.socrata.com/api/catalog/v1/domains/data.cityofchicago.org/facets
+
+
 # http://api.us.socrata.com/api/catalog/v1?domains=datacatalog.cookcountyil.gov&search_context=datacatalog.cookcountyil.gov&limit=600
 
 # http://api.us.socrata.com/api/catalog/v1?categories=Education&tags=families&search_context=data.seattle.gov
