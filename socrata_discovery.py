16fe384d6510d6eba63fb63a4082c368419013a7
alxfed <alxfed@gmail.com>
2019-05-08 16:57:41 -0500

    discovery

--- socrata_discovery.py
+++ socrata_discovery.py
@@ -2,6 +2,7 @@
 Socrata discovery API
 """
 
-# http://api.us.socrata.com/api/catalog/v1?domains=datacatalog.cookcountyil.gov&search_context=datacatalog.cookcountyil.gov
+# http://api.us.socrata.com/api/catalog/v1?domains=datacatalog.cookcountyil.gov&search_context=datacatalog.cookcountyil.gov&limit=600
 
 # http://api.us.socrata.com/api/catalog/v1?categories=Education&tags=families&search_context=data.seattle.gov
+
