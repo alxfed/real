8f7a44a0e5a5a6d98cd6e1fe682d1410c69b8292
alxfed <alxfed@gmail.com>
2019-05-11 09:03:11 -0500

    reits

---
+++ legistar_chicago.py
@@ -0,0 +1,9 @@
+"""
+Legistar and zoning
+"""
+
+#https://webapi.legistar.com
+
+#https://chicago.legistar.com/LegislationDetail.aspx?ID=2785572&GUID=EE721449-1592-4051-A844-CD29042C3147&Options=Advanced&Search=&fbclid=IwAR0c5AORJZ1s_29nx1aU9yIt9LyzP6Mv5jN4dBGp5EzCqxa5ZFrisTTb8aI
+
+#http://webapi.legistar.com/v1/chicago/matters/90193
