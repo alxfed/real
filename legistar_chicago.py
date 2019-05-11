9b8b6a3973e96169d11671f0b4b01cb9dcfc5811
alxfed <alxfed@gmail.com>
2019-05-11 09:13:43 -0500

    legistar

--- legistar_chicago.py
+++ legistar_chicago.py
@@ -2,6 +2,24 @@
 Legistar and zoning
 """
 
+'''
+Changes of zoning in City Council monthly meetings
+
+12.Presentation of petitions, communication, resolutions, orders, and
+ordinances introduced by Aldermen.
+The City Clerk will "Call the Wards" and read into the record new legislative
+proposals (ordinances, orders, petitions, resolutions, or other original matters)
+introduced by aldermen on a variety of topics including but not limited to
+Municipal Code amendments, traffic regulations, zoning matters, licensing
+requirements, etc. The order of presentation alternates each meeting beginning
+with the 1st Ward on one meeting, then the 50th Ward on the next succeeding
+meeting, and so forth.
+Under City Council Rules, all legislative matters introduced are automatically
+referred to a City Council Committee without debate, unless there is a 2/3 vote of
+the members (34 votes are required) to suspend the rules to allow immediate
+consideration of the matter in question.
+'''
+
 #https://webapi.legistar.com
 
 #https://chicago.legistar.com/LegislationDetail.aspx?ID=2785572&GUID=EE721449-1592-4051-A844-CD29042C3147&Options=Advanced&Search=&fbclid=IwAR0c5AORJZ1s_29nx1aU9yIt9LyzP6Mv5jN4dBGp5EzCqxa5ZFrisTTb8aI
