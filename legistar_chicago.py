ce929c98a8f91ed1d2570865fc4da896a8f0a16c
alxfed <alxfed@gmail.com>
2019-05-11 09:37:47 -0500

    legistar

--- legistar_chicago.py
+++ legistar_chicago.py
@@ -25,3 +25,10 @@ consideration of the matter in question.
 #https://chicago.legistar.com/LegislationDetail.aspx?ID=2785572&GUID=EE721449-1592-4051-A844-CD29042C3147&Options=Advanced&Search=&fbclid=IwAR0c5AORJZ1s_29nx1aU9yIt9LyzP6Mv5jN4dBGp5EzCqxa5ZFrisTTb8aI
 
 #http://webapi.legistar.com/v1/chicago/matters/90193
+
+'''
+Type: Ordinance
+Status: Passed
+Topic: ZONING RECLASSIFICATIONS
+Ward/office: 1 to 50
+'''
