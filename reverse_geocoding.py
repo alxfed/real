f3eda2581215fd864dc266365b9741e2113e1aad
alxfed <alxfed@gmail.com>
2019-05-12 18:43:01 -0500

    reverse geocoding

---
+++ reverse_geocoding.py
@@ -0,0 +1,13 @@
+"""
+Reverse geocoding
+"""
+
+# https://geoservices.tamu.edu/Services/ReverseGeocoding/WebService/v04_01/HTTP.aspx
+
+# https://www.pitneybowes.com/us/location-intelligence/geocoding-data-enrichment/spectrum-geocoding-on-demand.html
+
+# https://developers.arcgis.com/rest/geocode/api-reference/geocoding-reverse-geocode.htm
+
+# https://developer.mapquest.com/documentation/open/nominatim-search/
+
+# https://nominatim.openstreetmap.org/
